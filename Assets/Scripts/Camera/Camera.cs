e0f65bfb0975988a0048a0cd37e8ca290a7cba1b
jalexanderhc <71862518+jalexanderhc@users.noreply.github.com>
2020-11-18 00:41:55 -0500

    Movimiento Camara Funcional
    Movimiento Camara Funcional

--- Assets/Scripts/Camera/Camera.cs
+++ Assets/Scripts/Camera/Camera.cs
@@ -4,36 +4,21 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    public GameObject camara;
-    public GameObject destino;
-    public Vector3 posicionCamara;
-    public Vector3 posicionDestino;
-    public static float tiempo;
-
-    public Transform transformDestino; //Refrencia a la posicion destino
+    public Transform TransformDestino; //refrencia a la posicion destino
     public bool UsarTiempo; //Si no usa tiempo usa velocidad
     public float Tiempo; //Tiempo en recorrer la distancia
     public float Velocidad; //A que velocidad //Solo se usa si UsarTiempo esta activo
 
-    public float rateTiempo;
-    public float rateVelocity;
-    public float rate; //El rate o factor movimiento que realmente se utilizara
-    public float t = 0.0f; //ayuda
+    float rateTiempo;
+    float rateVelocity;
+    float rate; //El rate o factor movimiento que realmente se utilizara
+    float t = 0.0f; //ayuda
 
+    Vector3 startPos;
 
-    // Start is called before the first frame update
     void Start()
     {
-        camara = GameObject.Find("Main Camera");
-        posicionCamara = camara.transform.position;
-
-        destino = GameObject.Find("Destino");
-        posicionDestino = destino.transform.position;
-
-        Tiempo = 1.2f;
-        UnityEngine.Debug.Log("posicionCamara :" + posicionCamara);
-        UnityEngine.Debug.Log("posicionDestino :" + posicionDestino);
-
+        startPos = transform.position; //Mi punto inicial del personaje
         if (UsarTiempo)
         {
             float rateTiempo = 1f / Tiempo; //Calculamos cuanto nos moveremos para comprir la distancia siempre en el mismo tiempo
@@ -42,19 +27,17 @@ public class Camera : MonoBehaviour
         else //Entonces nos moveremos a una velocidad fija
         {
             //Calculamos cuanto tenemos que movernos para movernos a una velocidad fija
-            float rateVelocity = 1f / Vector3.Distance(posicionCamara, posicionDestino) * Velocidad;
+            float rateVelocity = 1f / Vector3.Distance(startPos, TransformDestino.position) * Velocidad;
             rate = rateVelocity;
         }
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (t <= 1f)
         {
             t += Time.deltaTime * rate;
-            transform.position = Vector3.Lerp(posicionCamara, posicionDestino, t);
+            transform.position = Vector3.Lerp(startPos, TransformDestino.position, t);
         }
     }
 }
