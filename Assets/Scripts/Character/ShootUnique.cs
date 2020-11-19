830e6772c339d0ace88788861b7674126a5a440f
DavidS-Codes <dspp0107@hotmail.com>
2020-11-19 14:22:29 -0500

    Shoot Character destroy tile
    Shoot destroy tile point from tilemap

--- Assets/Scripts/Character/ShootUnique.cs
+++ Assets/Scripts/Character/ShootUnique.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ShootUnique : MonoBehaviour
 {
@@ -9,16 +10,34 @@ public class ShootUnique : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //rb.velocity = transform.right * speed;
+        
         //The direction to shoot the bullet
-        Vector3 pos = rb.transform.forward * speed;
+        //Vector3 pos = rb.transform.up * speed;
+        Vector3 pos = new Vector3( 1 * speed, 1*speed,0);
         //Shoot
         rb.velocity = pos;
     }
 
     // Update is called once per frame
-   void OnTriggerEnter2D()
+   void OnCollisionEnter2D(Collision2D coll)
+
     {
+
         Destroy(gameObject);
+        Tilemap tilemap = coll.gameObject.GetComponent<Tilemap>();
+        //Debug.Log(coll.gameObject);
+        foreach (ContactPoint2D hit in coll.contacts)
+        {
+            Vector2 hitPoint = hit.point;
+            //Debug.Log(hit.collider.transform.name);
+            //Debug.Log(hitPoint);
+            //Debug.Log(tilemap.WorldToCell(hitPosition));
+            tilemap.SetTile(tilemap.WorldToCell(hitPoint), null);
+        }
+
+
     }
+
+
+    
 }
