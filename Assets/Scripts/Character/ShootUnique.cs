b50183d8eaa2096b2803a08a502582c86c5e87fa
solbermudez <solangibermudez10@gmail.com>
2020-11-19 22:25:38 -0500

    ProyectoBase19112020
    Cambios de Camara
    Ataque personaje
    Reloj solito
    
    Co-Authored-By: jalexanderhc <71862518+jalexanderhc@users.noreply.github.com>
    Co-Authored-By: DavidS-Codes <53781936+DavidS-Codes@users.noreply.github.com>

--- Assets/Scripts/Character/ShootUnique.cs
+++ Assets/Scripts/Character/ShootUnique.cs
@@ -20,13 +20,14 @@ public class ShootUnique : MonoBehaviour
     // Update is called once per frame
   void OnCollisionEnter2D(Collision2D coll)
     {
+        Destroy(gameObject);
          Tilemap tilemap = coll.gameObject.GetComponent<Tilemap>();
         //Debug.Log(coll.gameObject);
         foreach (ContactPoint2D hit in coll.contacts)
         {
             Vector2 hitPoint = hit.point;
             //Debug.Log(hit.collider.transform.name);
-            //Debug.Log(hitPoint);
+            Debug.Log(hitPoint);
             //Debug.Log(tilemap.WorldToCell(hitPosition));
             tilemap.SetTile(tilemap.WorldToCell(hitPoint), null);
         }
