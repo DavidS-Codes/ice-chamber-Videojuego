d053b577f3950d4ab65b8f46e4299f9bb4ce46c0
solbermudez <solangibermudez10@gmail.com>
2020-11-25 00:01:51 -0500

    Animacion Enemigos y bandera

---
+++ Assets/Scripts/Flag/Flag.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Flag : MonoBehaviour
+{
+    public Animator anim;
+
+    private void OnTriggerEnter2D(Collider2D colider)
+    {
+
+        if (colider.gameObject.transform.root.gameObject.CompareTag("Player"))
+        {
+            anim.SetBool("IniciarAnimacion", true);
+        }
+    }
+
+    void finalizarJuego()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
